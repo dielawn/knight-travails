8d00c98d61d82cf4724e678959daddb322a24a1a
dielawn <dielawnmerkill@gmail.com>
2023-08-06 05:36:31 -0600

    basic chessboard from a class

--- script.js
+++ script.js
@@ -1,1 +1,24 @@
 const containerDiv = document.getElementById('container')
+
+class Knight {
+
+}
+
+class ChessBoard {
+    constructor() {
+        this.yAxis = ['1','2','3','4','5','6','7','8']
+        this.xAxis = ['a','b','c','d','e','f','g','h']
+    }
+    createBoard() {
+        const squareId = []
+        for (let i = 0; i < this.yAxis.length; i++) {
+            for (let j = 0; j < this.xAxis.length; j++) {
+                squareId.push(this.xAxis[i] + this.yAxis[j])
+            }
+        }
+        console.log(squareId)
+    }
+}
+
+const chessBoard = new ChessBoard()
+chessBoard.createBoard()
